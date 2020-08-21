2c3d497631a4f1bddf6d538818f20d7c630c1592
Lourine <lourine.millicent@gmail.com>
2020-08-21 16:42:55 +0300

    Add custopm pipe dateAgo to display how much time has passed since the quote was created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { DateAgoPipe } from './date-ago.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    QuoteDetailsComponent
+    QuoteDetailsComponent,
+    DateAgoPipe
   ],
   imports: [
     BrowserModule,
